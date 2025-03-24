29936fcc96c0edf4d5db541658f2598c0ab6f393
lowbudgetgadgets <lowbudgetgadgets123@gmail.com>
2025-03-24 12:42:50 +0530

    add .env .gitignore script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('gaurav');
+
+import chalk from 'chalk'
+const error = chalk.bold.red;
+const warning = chalk.hex('#FFA500'); // Orange color
+
+console.log(error('Error!'));
+console.log(warning('Warning!'));
+console.log(chalk.red("Gaurav")+' '+ chalk.blue('Sharma'));
+
+import { upperCase} from 'upper-case';
+console.log(upperCase('Gaurav'));
+
+import { lowerCase} from 'lower-case';
+console.log(lowerCase('SHARMA'));
